35a77e16a5dc02f9708c905b25a8ab3e6b097551
tsetoung <jtmoua@yahoo.com>
2015-06-04 22:06:08 -0400

    added HTTParty back

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def about
+    nytimes = HTTParty.get("http://quotesondesign.com/api/3.0/api-3.0.json?callback=my_function")
+    @results = nytimes
   end
 end
